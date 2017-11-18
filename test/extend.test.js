a4120ef1223ed0983a81088f3d628b0a02dfc987
yupeng.kuo <yupeng.kuo@gmail.com>
2017-11-19 00:23:31 +0800

    init: project initialized

---
+++ test/extend.test.js
@@ -0,0 +1,44 @@
+const assert = require('assert')
+const extend = require('../index')
+const describe = function (description, func) {
+  if (func.length === 1) {
+    const done = function (err) {
+      if (err) {
+        return console.error('\x1b[31m%s\n  %s\x1b[0m', description, err.stack)
+      }
+      console.log('\x1b[32m%s\x1b[0m', description)
+    }
+    return func(done)
+  }
+  try {
+    func()
+    console.log('\x1b[32m%s\x1b[0m', description)
+  } catch (err) {
+    console.error('\x1b[31m%s\n  %s\x1b[0m', description, err.stack)
+  }
+}
+
+describe('test extend.js', function () {
+  // describe('test mkdir()', function (done) {
+  //   const fullpath = '/Users/yupeng.kuo/demo/demo/1/2/3/4'
+  //   extend.mkdir(fullpath, done)
+  // })
+  describe('test mkdirSync()', function () {
+    const fullpath = '/Users/yupeng.kuo/demo/demo/1/2/3/4'
+    extend.mkdirSync(fullpath)
+  })
+  describe('test rmSync()', function () {
+    const fullpath = '/Users/yupeng.kuo/demo'
+    extend.rmSync(fullpath)
+  })
+  describe('test find()', function (done) {
+    const fullpath = '..'
+    extend.find(fullpath, /^huodong/, function (err, result) {
+      if (err) {
+        return done(err)
+      }
+      console.log(result)
+      done()
+    })
+  })
+})
